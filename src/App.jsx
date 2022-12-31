c1239e49389a2683262b7596752ae61055b7e738
Samyak Khatua <samyakkhatua@duck.com>
2022-12-31 21:51:58 +0530

    CSS removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import { useState } from 'react'
-// import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="text-3xl font-bold underline">
       Hi PDF
     </div>
   )
